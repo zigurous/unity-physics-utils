b030d70686691afe3f0f9fb08c0c5024caae4587
Adam Graham <adam@zigurous.com>
2021-02-13 23:27:15 -0600

    Add utility layer mask extensions

---
+++ Runtime/Scripts/LayerMaskExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Physics
+{
+    public static class LayerMaskExtensions
+    {
+        public static bool Contains(this LayerMask mask, int layer)
+        {
+            return mask == (mask | (1 << layer));
+        }
+
+    }
+
+}
