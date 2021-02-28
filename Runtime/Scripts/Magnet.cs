f703ddffe6d275ab3efaa195e602b51b1034bea1
Adam Graham <adam@zigurous.com>
2021-02-28 02:55:30 -0600

    Rewrite magnet scripts with proper separation of responsibility

--- Runtime/Scripts/Magnet.cs
+++ Runtime/Scripts/Magnet.cs
@@ -2,9 +2,67 @@
 
 namespace Zigurous.Physics
 {
-    public sealed class Magnet : MonoBehaviour
+    /// <summary>
+    /// An object that produces a magnetic field that attracts objects.
+    /// </summary>
+    public class Magnet : MonoBehaviour
     {
+        /// <summary>
+        /// The strength of the magnet. If an object is within multiple magnetic
+        /// fields, then it will be attracted to the magnet with the higher
+        /// strength.
+        /// </summary>
+        [Tooltip("The strength of the magnet. If an object is within multiple magnetic fields, then it will be attracted to the magnet with the higher strength.")]
         public float strength = 1.0f;
+
+        /// <summary>
+        /// The radius of the magnetic field produced by the magnet. Magnetic
+        /// objects within the radius are attracted to the magnet.
+        /// </summary>
+        [Tooltip("The radius of the magnetic field produced by the magnet. Magnetic objects within the radius are attracted to the magnet.")]
+        [SerializeField]
+        private float _fieldRadius = 10.0f;
+
+        /// <summary>
+        /// The radius of the magnetic field produced by the magnet. Magnetic
+        /// objects within the radius are attracted to the magnet.
+        /// </summary>
+        public float fieldRadius
+        {
+            get => _fieldRadius;
+            set
+            {
+                _fieldRadius = value;
+                this.magneticField.radius = value;
+            }
+        }
+
+        /// <summary>
+        /// The sphere collider that represents the magnetic field of the
+        /// magnet.
+        /// </summary>
+        public SphereCollider magneticField { get; protected set; }
+
+        protected virtual void Awake()
+        {
+            this.magneticField = this.gameObject.AddComponent<SphereCollider>();
+            this.magneticField.isTrigger = true;
+            this.magneticField.radius = _fieldRadius;
+            this.magneticField.hideFlags = HideFlags.HideInInspector;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            this.magneticField = null;
+        }
+
+        protected virtual void OnValidate()
+        {
+            if (this.magneticField != null) {
+                this.magneticField.radius = _fieldRadius;
+            }
+        }
+
     }
 
 }
---
+++ Runtime/Scripts/Magnetic.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace Zigurous.Physics
+{
+    /// <summary>
+    /// An object with magnetic properties that can be attracted to a magnet.
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody))]
+    public class Magnetic : MonoBehaviour
+    {
+        /// <summary>
+        /// The magnet the object is currently attracted to.
+        /// </summary>
+        public Magnet attractedMagnet { get; protected set; }
+
+        /// <summary>
+        /// The rigidbody of the magnetic object that handles moving the object
+        /// towards the magnet when attracted.
+        /// </summary>
+        public new Rigidbody rigidbody { get; protected set; }
+
+        protected virtual void Awake()
+        {
+            this.rigidbody = GetComponent<Rigidbody>();
+        }
+
+        protected virtual void OnDestroy()
+        {
+            this.attractedMagnet = null;
+            this.rigidbody = null;
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            if (this.attractedMagnet != null)
+            {
+                Vector3 direction = this.attractedMagnet.transform.position - this.transform.position;
+                float index = (this.attractedMagnet.magneticField.radius - direction.magnitude) / this.attractedMagnet.magneticField.radius;
+                this.rigidbody.AddForce(this.attractedMagnet.strength * direction * index);
+            }
+        }
+
+        protected virtual void OnTriggerEnter(Collider other)
+        {
+            Magnet magnet = other.GetComponent<Magnet>();
+
+            if (magnet != null)
+            {
+                if (this.attractedMagnet == null) {
+                    this.attractedMagnet = magnet;
+                } else if (magnet.strength > this.attractedMagnet.strength) {
+                    this.attractedMagnet = magnet;
+                }
+            }
+        }
+
+        protected virtual void OnTriggerExit(Collider other)
+        {
+            Magnet magnet = other.GetComponent<Magnet>();
+
+            if (magnet != null && this.attractedMagnet == magnet) {
+                this.attractedMagnet = null;
+            }
+        }
+
+    }
+
+}
