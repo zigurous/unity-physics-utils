851dd658317947f5eb4cbb4866cad40c0cfc2d6c
Adam Graham <adam@zigurous.com>
2021-07-14 05:32:55 -0500

    Add new subclasses for custom unity events

---
+++ Runtime/Events/ColliderEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Zigurous.Physics.Events
+{
+    /// <summary>
+    /// A custom unity event for use with the Collider class.
+    /// </summary>
+    [System.Serializable]
+    public class ColliderEvent : UnityEvent<Collider>
+    {
+    }
+
+}
---
+++ Runtime/Events/CollisionEvent2D.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Zigurous.Physics.Events
+{
+    /// <summary>
+    /// A custom unity event for use with the Collision2D class.
+    /// </summary>
+    [System.Serializable]
+    public class CollisionEvent2D : UnityEvent<Collision2D>
+    {
+    }
+
+}
